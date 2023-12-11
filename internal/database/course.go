525f3c64ff3f8203e945906a31371b91e9329d69
Marcos Augusto <msaorc@hotmail.com>
2023-12-11 09:42:04 -0300

    Criação do CourseDB

--- internal/database/course.go
+++ internal/database/course.go
@@ -1,1 +1,34 @@
 package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type Course struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+	CategoryID  string
+}
+
+func NewCourse(db *sql.DB) *Course {
+	return &Course{db: db}
+}
+
+func (c *Course) create(name string, description string, categoryID string) (Course, error) {
+	id := uuid.New().String()
+	_, err := c.db.Exec("INSERT INTO category (id, name, description, category_id) VALUES ($1, $2, $3, $4)",
+		id, name, description, categoryID)
+	if err != nil {
+		return Course{}, err
+	}
+	return Course{
+		ID:          id,
+		Name:        name,
+		Description: description,
+		CategoryID:  categoryID,
+	}, nil
+}
