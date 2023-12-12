eb6c68fed83e33f7c1e4a844ef283c5fe21dc8b4
Marcos Augusto <msaorc@hotmail.com>
2023-12-12 08:54:25 -0300

    Criando relacionamento entre categorias e cursos

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,20 +2,6 @@
 
 package model
 
-type Category struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Courses     []*Course `json:"courses"`
-}
-
-type Course struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Category    *Category `json:"category"`
-}
-
 type NewCategory struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
